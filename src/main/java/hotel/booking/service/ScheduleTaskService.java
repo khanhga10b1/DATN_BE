3876a1feb7dd9e130b3e78ffddc1773dfa38f162
khanhga10b1 <hokhanha2@gmail.com>
2022-03-07 12:57:05 +0700

    heroku schedule

---
+++ src/main/java/hotel/booking/service/ScheduleTaskService.java
@@ -0,0 +1,6 @@
+package hotel.booking.service;
+
+public interface ScheduleTaskService {
+
+    void scheduleTaskWithFixedRate();
+}
---
+++ src/main/java/hotel/booking/service/ScheduleTaskServiceImpl.java
@@ -0,0 +1,34 @@
+package hotel.booking.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class ScheduleTaskServiceImpl implements ScheduleTaskService {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
+    @Value("${api-url}")
+    private String apiUrl;
+
+
+    @Override
+    @Scheduled(fixedRate = 900000)
+    public void scheduleTaskWithFixedRate() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+        headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            ResponseEntity<String> responseEntity = restTemplate.exchange(apiUrl+"/test", HttpMethod.GET, httpEntity, String.class);
+            logger.info(responseEntity.getBody());
+        } catch (Exception exception) {
+            logger.info("call k dc");
+        }
+    }
+}
